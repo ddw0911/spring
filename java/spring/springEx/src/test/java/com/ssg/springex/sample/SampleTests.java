d1ce6a2430d788beac8ec9dcac32f7a9f279d62e
dongwoo <dongu044@gmail.com>
2024-09-12 16:57:06 +0900

    스프링 웹 MVC 2일차 수업내용

--- java/spring/springEx/src/test/java/com/ssg/springex/sample/SampleTests.java
+++ java/spring/springEx/src/test/java/com/ssg/springex/sample/SampleTests.java
@@ -9,7 +9,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 @Log4j2
 @ExtendWith(SpringExtension.class)
-@ContextConfiguration(locations="file:src/main/webapp/WEB-INF/root-context.xml")
+@ContextConfiguration(locations="file:../../../../../../../../Spring-TodoService/src/main/webapp/WEB-INF/root-context.xml")
 public class SampleTests {
 
     @Autowired
